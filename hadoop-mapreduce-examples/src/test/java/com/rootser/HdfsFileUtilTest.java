c459b6d7f40ca01d8845bb96fb8dc0c206106801
john <jhancock1975@gmail.com>
2014-08-10 15:44:40 -0400

    add interface for HdfsFilUtil

--- hadoop-mapreduce-examples/src/test/java/com/rootser/HdfsFileUtilTest.java
+++ hadoop-mapreduce-examples/src/test/java/com/rootser/HdfsFileUtilTest.java
@@ -32,9 +32,12 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:HdfsAppContext.xml" })
 public class HdfsFileUtilTest {
+
 	@Autowired
-	 HdfsFileUtil util;
+	HdfsFileUtil util;
+
 	static String hdfsUrlStr;
+
 	@BeforeClass
 	/**
 	 * this keeps the test flexible
@@ -43,103 +46,115 @@ public class HdfsFileUtilTest {
 	 * 
 	 * @throws IOException
 	 */
-	public static void setUp() throws IOException{
-		InputStream inputStream = HdfsFileUtilTest.class.getClassLoader().getResourceAsStream("hdfs.properties");
+	public static void setUp() throws IOException {
+		InputStream inputStream = HdfsFileUtilTest.class.getClassLoader()
+				.getResourceAsStream("hdfs.properties");
 		Properties prop = new Properties();
 		prop.load(inputStream);
 		hdfsUrlStr = prop.getProperty("HDFS_URL");
 	}
+
 	private static String test1FileName = "/tmp/test.txt";
+
 	@Test
-	public void writeTest() throws IOException{
-		
+	public void writeTest() throws IOException {
+
 		BufferedWriter br = util.openBufferedWriter(test2FileName);
-		String testStr = "hello much improved file system object." ;
+		String testStr = "hello much improved file system object.";
 		br.write(testStr);
 		br.close();
 		Path myfile = new Path(hdfsUrlStr + test2FileName);
 		List<String> results = readLines(myfile, new Configuration());
 		assertTrue(results.get(0).equals(testStr));
 	}
-	
+
 	/**
 	 * copied from
-	 * http://blog.matthewrathbone.com/2013/12/28/Reading-data-from-HDFS-even-if-it-is-compressed.htmll
-	 * need separate logic for testing util, so we're not hiding bugs
+	 * http://blog.matthewrathbone.com/2013/12/28/Reading-data-from-HDFS
+	 * -even-if-it-is-compressed.htmll need separate logic for testing util, so
+	 * we're not hiding bugs
 	 * 
 	 * @param location
 	 * @param conf
 	 * @return
 	 * @throws IOException
 	 */
-	public List<String> readLines(Path location, Configuration conf) throws IOException {
-	    FileSystem fileSystem = FileSystem.get(location.toUri(), conf);
-	    CompressionCodecFactory factory = new CompressionCodecFactory(conf);
-	    FileStatus[] items = fileSystem.listStatus(location);
-	    if (items == null) return new ArrayList<String>();
-	    List<String> results = new ArrayList<String>();
-	    for(FileStatus item: items) {
+	public List<String> readLines(Path location, Configuration conf)
+			throws IOException {
+		FileSystem fileSystem = FileSystem.get(location.toUri(), conf);
+		CompressionCodecFactory factory = new CompressionCodecFactory(conf);
+		FileStatus[] items = fileSystem.listStatus(location);
+		if (items == null)
+			return new ArrayList<String>();
+		List<String> results = new ArrayList<String>();
+		for (FileStatus item : items) {
 
-	      // ignoring files like _SUCCESS
-	      if(item.getPath().getName().startsWith("_")) {
-	        continue;
-	      }
+			// ignoring files like _SUCCESS
+			if (item.getPath().getName().startsWith("_")) {
+				continue;
+			}
 
-	      CompressionCodec codec = factory.getCodec(item.getPath());
-	      InputStream stream = null;
+			CompressionCodec codec = factory.getCodec(item.getPath());
+			InputStream stream = null;
 
-	      // check if we have a compression codec we need to use
-	      if (codec != null) {
-	        stream = codec.createInputStream(fileSystem.open(item.getPath()));
-	      }
-	      else {
-	        stream = fileSystem.open(item.getPath());
-	      }
+			// check if we have a compression codec we need to use
+			if (codec != null) {
+				stream = codec
+						.createInputStream(fileSystem.open(item.getPath()));
+			} else {
+				stream = fileSystem.open(item.getPath());
+			}
 
-	      StringWriter writer = new StringWriter();
-	      IOUtils.copy(stream, writer, "UTF-8");
-	      String raw = writer.toString();
-	      String[] resulting = raw.split("\n");
-	      for(String str: resulting) {
-	        results.add(str);
-	      }
-	    }
-	    return results;
-	  }
+			StringWriter writer = new StringWriter();
+			IOUtils.copy(stream, writer, "UTF-8");
+			String raw = writer.toString();
+			String[] resulting = raw.split("\n");
+			for (String str : resulting) {
+				results.add(str);
+			}
+		}
+		return results;
+	}
+
 	static String test2FileName = "test2.txt";
 	static String test2Str = "hello world";
+
 	@Test
-	public void readTest() throws IOException, URISyntaxException{
-		
+	public void readTest() throws IOException, URISyntaxException {
+
 		writeToFile(test2FileName, test2Str);
 		List<String> actual = util.readLines(test2FileName);
 		assertTrue(actual.get(0).equals(test2Str));
 	}
-	
-	private void writeToFile(String fileName, String textToWrite) throws IOException, URISyntaxException{
+
+	private void writeToFile(String fileName, String textToWrite)
+			throws IOException, URISyntaxException {
 		Configuration configuration = new Configuration();
-		FileSystem hdfs = FileSystem.get( new URI( hdfsUrlStr ), configuration );
+		FileSystem hdfs = FileSystem.get(new URI(hdfsUrlStr), configuration);
 		Path file = new Path(hdfsUrlStr + fileName);
-		if ( hdfs.exists( file )) {
-			hdfs.delete( file, true ); 
-			} 
-		OutputStream os = hdfs.create( file);
-		BufferedWriter br = new BufferedWriter( new OutputStreamWriter( os, "UTF-8" ) );
+		if (hdfs.exists(file)) {
+			hdfs.delete(file, true);
+		}
+		OutputStream os = hdfs.create(file);
+		BufferedWriter br = new BufferedWriter(new OutputStreamWriter(os,
+				"UTF-8"));
 		br.write(textToWrite);
 		br.close();
 	}
-	
+
 	@AfterClass
-	public static void cleanUp() throws IOException, URISyntaxException{
+	public static void cleanUp() throws IOException, URISyntaxException {
 		deleteFile(test2FileName);
 		deleteFile(test1FileName);
 	}
-	private static void deleteFile(String fileName)  throws IOException, URISyntaxException{
+
+	private static void deleteFile(String fileName) throws IOException,
+			URISyntaxException {
 		Configuration configuration = new Configuration();
-		FileSystem hdfs = FileSystem.get( new URI( hdfsUrlStr ), configuration );
+		FileSystem hdfs = FileSystem.get(new URI(hdfsUrlStr), configuration);
 		Path file = new Path(hdfsUrlStr + fileName);
-		if ( hdfs.exists( file )) {
-			hdfs.delete( file, true ); 
-			}
+		if (hdfs.exists(file)) {
+			hdfs.delete(file, true);
+		}
 	}
 }
